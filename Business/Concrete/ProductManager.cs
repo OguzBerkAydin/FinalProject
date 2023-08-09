ccd91428862f58a2cd668051855f0989ebd372ca
OGUZ BERK AYDIN <ouzberkaydin@gmail.com>
2023-08-09 22:05:01 +0300

    fluent validation added

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -1,10 +1,13 @@
 ﻿using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -41,6 +44,10 @@ namespace Business.Concrete
 
         public IResult Add(Product product)
         {
+
+          ValidationTool.Validate(new ProductValidator(), product);
+
+
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
